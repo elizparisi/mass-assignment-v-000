bd98350035d4391a5232e64faade911dbd85f9cb
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-02-19 23:49:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,19 +3,8 @@ class Person
   
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def intialize (attname = name
-    @birthday = birthday
-    @hair_color = hair_color
-    @eye_color = eye_color
-    @height = height
-    @weight = weight
-    @handed = handed 
-    @complexion = complexion
-    @t_shirt_size = t_shirt_size
-    @wrist_size = wrist_size 
-    @glove_size = glove_size
-    @pant_lenght = pant_length
-    @pant_width = pant_width
+  def intialize(attributes)
+    @attributes = {}
     
   end
 end
